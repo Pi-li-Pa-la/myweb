556f70443d84a442036d22147413326de922c9d7
zxpilipala <eric@example.com>
2018-01-23 21:40:22 +0800

    reset mysql password

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ secret_key = 'zxpilipala'
 
 # _db_path = 'db.sqlite'
 # db_uri = 'sqlite:///{}'.format(_db_path)
-db_uri = 'mysql+pymysql://system:manager/myweb'
+db_uri = 'mysql+pymysql://root:8589934592@localhost/myweb'
