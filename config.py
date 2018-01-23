15952eb75cf514fa627096a5653611dd2cd9bda5
zxpilipala <eric@example.com>
2018-01-23 21:14:17 +0800

    just a joke

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ secret_key = 'zxpilipala'
 
 # _db_path = 'db.sqlite'
 # db_uri = 'sqlite:///{}'.format(_db_path)
-db_uri = 'mysql+pymysql://system:manager@localhost/myweb'
+db_uri = 'mysql+pymysql://system:manager/myweb'
