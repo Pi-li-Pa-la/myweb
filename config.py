0e1088846f94503c5c0630c4fcfe0b566d86e6b5
zxpilipala <eric@example.com>
2018-01-23 20:43:37 +0800

    modify config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 secret_key = 'zxpilipala'
 
-_db_path = 'db.sqlite'
-db_uri = 'sqlite:///{}'.format(_db_path)
+# _db_path = 'db.sqlite'
+# db_uri = 'sqlite:///{}'.format(_db_path)
+db_uri = 'mysql+pymysql://root:8589934592@localhost/myweb'
